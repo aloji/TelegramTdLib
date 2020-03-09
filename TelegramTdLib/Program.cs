2931045868df358ff01729508fa5ed17f816a788
Alain <alain@fxstreet.com>
2020-03-09 11:30:39 +0100

    Add project files.

---
+++ TelegramTdLib/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TdLib;
+
+namespace TelegramTdLib
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            try
+            {
+                TdLog.SetVerbosityLevel(0);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("End");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
